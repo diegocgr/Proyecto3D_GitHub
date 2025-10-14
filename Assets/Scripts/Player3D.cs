3975e5df36f93a3be29ba19d0ce47cf3e625286c
diegocgr <diegocgr@gmail.com>
2025-10-14 19:33:01 +0200

    Creación de carpeta Scripts y script Player3D

---
+++ Assets/Scripts/Player3D.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Player3D : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Player3D existe");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
